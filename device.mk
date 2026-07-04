8ef21860edbcc7fa4bd946537235561374cd9e9b
ermyltsor <lamiimuk@163.com>
2026-07-04 11:54:22 +0800

    RE546F: Introduce Data Decrypt

--- device.mk
+++ device.mk
@@ -8,6 +8,11 @@
 PRODUCT_COPY_FILES += \
     $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
 
+# Decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
 # Display	
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/commonsys-intf/display
